7572f45e91090e74cad428b264e57a3679688d6e
Jose Gonzalez <coding.tamalito@gmail.com>
2024-04-15 16:08:41 +0200

    fix parsing the cookies

--- src/utils/cookie_parser.rs
+++ src/utils/cookie_parser.rs
@@ -1,12 +1,12 @@
 
 
-pub struct MyCookie<'a> {
-    key: &'a str,
-    value: &'a str
+pub struct MyCookie {
+    key: String,
+    value: String
 }
 
-impl<'a> MyCookie<'a> {
-    fn new(key: &'a str, value: &'a str) -> MyCookie<'a> {
+impl MyCookie {
+    fn new(key: String, value: String) -> MyCookie {
         MyCookie {
             key: key,
             value: value
@@ -16,16 +16,59 @@ impl<'a> MyCookie<'a> {
 
 
 
-pub fn parse_cookies(value:&str) {
-
+pub fn parse_cookies(value:&str) -> Vec<MyCookie> {
+    let mut result: Vec<MyCookie> = Vec::new();
     // get the length
-    let len = value.len();
+    let len = value.len() - 1;
     println!("{}", len);
+    let mut key_cookie = String::from("");
+    let mut value_cookie = String::from("");
+    let mut found_equal = false;
     // iterate through all the chars
-    for (size, c) in value.char_indices() {
-        println!("{} {} {} {}", String::from("index"), size, String::from("char"), c);
+    for (index, c) in value.char_indices() {
+        //println!("{} {} {} {}", String::from("index"), index, String::from("char"), c);
+        // chekc if it is the space
+        if c == ' ' {
+            continue;
+        }
+        // check if it is the end of a key value pair
+        if c == ';' {
+            let key_clone = key_cookie.clone();
+            //let str_key: &str = key_clone.as_str();
+            let value_clone = value_cookie.clone();            
+            let cookie = MyCookie::new(key_clone, value_clone);
+            result.push(cookie);
+            key_cookie.clear();
+            value_cookie.clear();
+            continue;
+        }
+
+        if index == len {
+            // we have reached the end, create a MyCookie
+            value_cookie.push(c);
+            let key_clone = key_cookie.clone();
+            //let str_key = key_cookie.clone().as_str();
+            let value_clone = value_cookie.clone();            
+            let cookie = MyCookie::new(key_clone, value_clone);
+            result.push(cookie);
+            key_cookie.clear();
+            value_cookie.clear();
+            continue;
+        }
+
+        // check if there is an =
+        if c == '=' {
+            found_equal = true;
+            continue;
+        }
+        // check if it should go to the key or the value
+        if found_equal {
+            // push it to the value
+            value_cookie.push(c);
+        } else {
+            key_cookie.push(c);
+        }
         
-    }
-
-
+    } // end of for loop
+    result
 }
