191693ce42e063539897f031fd0dd6cd56d68506
Jose Gonzalez <coding.tamalito@gmail.com>
2024-06-15 08:47:38 +0200

    odify to retreive the access token from the database

--- src/utils/build_headers/build_authorization_header.rs
+++ src/utils/build_headers/build_authorization_header.rs
@@ -2,15 +2,19 @@ use awc::http::header::HeaderMap;
 use super::super::get_cookie::*;
 
 /// Retrieves the auth token from the cookie and creates the authorization header
-pub fn get_authorization_header(headers: &HeaderMap) -> (bool, String) {
+pub fn get_authorization_header(headers: &HeaderMap, possible_access_token: Option<String>) -> (bool, String) {
 
-    let auth_token = get_authorization_token_cookie(headers);
+    let mut auth_token = get_authorization_token_cookie(headers);
 
-    if auth_token.is_none() {
+    if auth_token.is_none() && possible_access_token.is_none() {
         // there is no token
         return (false, String::from("There is no header with the token"));
     }
-    println!("{:?}", auth_token);
+
+    if auth_token.is_none() && possible_access_token.is_some() {
+        auth_token = possible_access_token;
+    }
+    println!("line 17 build_authorization_headers {:?}", auth_token);
     // there is a token, create the &str
     let auth_header = format!("{}{}", "Bearer ", auth_token.unwrap());
     //let auth_header = auth_header.as_str();
