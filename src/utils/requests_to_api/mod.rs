f3d6bbde675d46e487d1548f29ae838772c58309
Jose Gonzalez <coding.tamalito@gmail.com>
2024-06-18 05:38:15 +0200

    add logic to refresh the post request

--- src/utils/requests_to_api/mod.rs
+++ src/utils/requests_to_api/mod.rs
@@ -2,3 +2,4 @@ pub mod put_request_empty;
 pub mod errors_spotify;
 pub mod post_request_empty;
 pub mod inital_check_for_users;
+pub mod refresh_post_empty_body;
---
+++ src/utils/requests_to_api/refresh_post_empty_body.rs
@@ -0,0 +1,24 @@
+use actix_web::{HttpRequest, HttpResponse};
+use crate::utils::refresh_token::refresh_token;
+use super::post_request_empty::post_request_emtpy_body;
+
+
+/// refreshes a token and sends a post request with empty body to the given url
+pub async fn refresh_and_send_post_empty_body(req: HttpRequest, auth_header: &str, url: &str) -> (bool, HttpResponse) {
+    println!("You need to refresh your token");
+    // try to refresh the token
+    let refreshed = refresh_token(req).await;
+    if refreshed {
+        println!("Token refreshed successfully");
+        // send the request again
+        let response_result_after_refreshed = post_request_emtpy_body(auth_header, url).await;
+        if response_result_after_refreshed.0 {
+            return (true, HttpResponse::NoContent().finish());
+        } else {
+            return (false, HttpResponse::BadRequest().finish());
+        }
+    }
+    println!("Token could not be refreshed");
+
+    (false, HttpResponse::BadRequest().finish())
+}
