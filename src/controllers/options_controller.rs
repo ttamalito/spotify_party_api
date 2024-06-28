7a9b0e9f089e11efad7dc46224e2200ae045cb85
Jose Gonzalez <coding.tamalito@gmail.com>
2024-06-28 17:37:10 +0200

    add options end point

--- src/controllers/options_controller.rs
+++ src/controllers/options_controller.rs
@@ -6,3 +6,9 @@ use actix_web::{web, App, HttpResponse, HttpServer, Responder, put, options};
 pub async fn options_shuffleOn() -> impl Responder {
     HttpResponse::Ok().finish()
 }
+
+#[options("shuffleOff")]
+pub async fn options_shuffleOff() -> impl Responder {
+    HttpResponse::Ok().finish()
+}
+
