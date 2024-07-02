b8763d2d8f2e44b157830335933745cf46654642
Jose Gonzalez <coding.tamalito@gmail.com>
2024-07-02 08:34:27 +0200

    hola

--- src/utils/structs_to_serialize_deserialize.rs
+++ src/utils/structs_to_serialize_deserialize.rs
@@ -1,9 +1,15 @@
 use mongodb::bson::oid::ObjectId;
-use serde::{Serialize};
+use serde::{Serialize, Deserialize};
 
 #[derive(Serialize, Debug)]
 pub struct ResponseForQueueToJoinParty {
     pub result: bool,
     pub message: String,
     pub users: Vec<ObjectId>
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ResponseUserHasParty {
+    pub result: bool,
+    pub party_id: String
 }
