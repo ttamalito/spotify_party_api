a9360c9e7df796f5b0e310675a8dedf28b0c2206
Jose Gonzalez <coding.tamalito@gmail.com>
2024-07-19 18:08:24 +0200

    add logic to refresh the token on getPlayBackState

---
+++ src/utils/requests_to_api/refresh_and_send_get_playback_state.rs
@@ -0,0 +1,24 @@
+use actix_web::{HttpRequest, HttpResponse};
+use crate::utils::refresh_token::refresh_token;
+use super::post_request_empty::post_request_emtpy_body;
+use super::get_request::get_request_get_play_back_state;
+use awc::http::StatusCode;
+use crate::utils::structs_to_serialize_deserialize::{ResponsePlaybackStateTrack, ResponsePlaybackStateEpisode};
+/// refreshes a token and sends a post request with empty body to the given url
+pub async fn refresh_and_send_get_playback_state(req: HttpRequest, auth_header: &str, url: &str) -> (bool, StatusCode, Option<ResponsePlaybackStateTrack>, Option<ResponsePlaybackStateEpisode>) {
+    
+    // try to refresh the token
+    let refreshed = refresh_token(req).await;
+    if refreshed {
+        // send the request again
+        let response_result_after_refreshed = get_request_get_play_back_state(auth_header, url).await;
+        if response_result_after_refreshed.0 {
+            return (true, response_result_after_refreshed.1, response_result_after_refreshed.2, response_result_after_refreshed.3);
+        } else {
+            return (false, StatusCode::BAD_REQUEST, None, None);
+        }
+    }
+    println!("Token could not be refreshed");
+
+    (false, StatusCode::BAD_REQUEST, None, None)
+}
